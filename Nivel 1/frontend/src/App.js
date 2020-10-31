a3e118265ad9abd91b1c1a9e552eb68d74c4f4ea
Yan <yankaiquecosta.yk@gmail.com>
2020-10-31 17:11:46 -0300

    Importando CSS e Imagens em ReactJS

--- Nivel 1/frontend/src/App.js
+++ Nivel 1/frontend/src/App.js
@@ -1,5 +1,7 @@
 import React,{useState} from 'react';
 import Header from './components/Header'
+import './app.css'
+import backgroundImage from './assets/background.jpg'
 
 function App(){
     const [projects, setProjects] = useState(['Desenvolvimento de app', 'desenvolvimento web'])
@@ -18,7 +20,7 @@ function App(){
             {projects.map(project => <li key={project}>{project}</li>)}
         </ul>
 
-
+        <img width={300} src={backgroundImage} />
         <button type="button" onClick={handleAddProject}>Adicionar projeto</button>
     </>
     )
