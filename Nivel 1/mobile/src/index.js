1170ccc5ee3e4c34858f6170e8854bd4c1eb2fc2
Yan <yankaiquecosta.yk@gmail.com>
2020-11-01 17:46:14 -0300

    Adicionando novos projetos com React Native

--- Nivel 1/mobile/src/index.js
+++ Nivel 1/mobile/src/index.js
@@ -1,5 +1,5 @@
 import React,{useEffect,useState} from 'react';
-import { SafeAreaView, FlatList, Text, StyleSheet, StatusBar } from 'react-native';
+import { SafeAreaView, FlatList, Text, StyleSheet, StatusBar, TouchableOpacity } from 'react-native';
 import api from './services/api';
 
 export default function App(){
@@ -9,6 +9,17 @@ export default function App(){
         api.get('projects').then(response=>{
             setProjects(response.data)})
     },[]);
+
+    async function handleAddProject(){
+        const response = await api.post('projects',{
+            title:`Front ${Date.now()}`,
+            owner:'Yan'
+        });
+
+        const project = response.data;
+
+        setProjects([...projects, project]);
+    }
 
     return(
         <>
@@ -26,6 +37,13 @@ export default function App(){
                         <Text style={styles.project}>{project.title}</Text>
                     )}
                 />
+                <TouchableOpacity 
+                    activeOpacity={0.6} 
+                    style={styles.button} 
+                    onPress={handleAddProject}
+                >
+                    <Text style={styles.buttonText}>Adicionar Projeto</Text>
+                </TouchableOpacity>
             </SafeAreaView>
         </>
     )
@@ -44,5 +62,17 @@ const styles = StyleSheet.create({
     project:{
         color:'white',
         fontSize: 30,
+    },
+    button:{
+        backgroundColor:'#FFF',
+        margin:20,
+        height: 50,
+        borderRadius:4,
+        justifyContent:'center',
+        alignItems:'center'
+    },
+    buttonText:{
+        fontWeight:'bold',
+        fontSize:16,
     }
 })
