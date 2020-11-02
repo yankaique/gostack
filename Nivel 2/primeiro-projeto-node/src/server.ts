85872b1cba6a7538fea639a9413244317790eeb5
Yan <yankaiquecosta.yk@gmail.com>
2020-11-02 18:49:06 -0300

    Debugando NodeJS

--- Nivel 2/primeiro-projeto-node/src/server.ts
+++ Nivel 2/primeiro-projeto-node/src/server.ts
@@ -1,11 +1,11 @@
 import express from 'express';
+import routes from './routes/index';
 
 const app = express();
 
-app.get('/',(request,response)=>{
-    return response.json({message:'Hello World'})
-})
+app.use(express.json());
+app.use(routes);
 
-app.listen(3333,()=>{
-    console.log('🚀 Server started on port 3333!')
-})
+app.listen(3333, () => {
+    console.log('🚀 Server started on port 3333!');
+});
